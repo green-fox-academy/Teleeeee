6af398e3badaf0320f79ef9bff02e974a9989846
Levente Paplogo <lev9433@gmail.com>
2019-09-11 18:03:58 +0200

    triangle

--- week-01/day-03/DrawTriangle/main.cpp
+++ week-01/day-03/DrawTriangle/main.cpp
@@ -5,8 +5,8 @@ int main() {
     std::cout << "Enter a number" << std::endl;
     std::cin >> num;
 
-    for (int i=1; i<=num; i++ ){
-        for (int j=1; j<=i; j++){
+    for (int i=1; i<=num; i = i + 2 ){
+        for (int j=1; j<=i; j = j +2){
             std::cout << "*";
         }
         std::cout << "\n";
