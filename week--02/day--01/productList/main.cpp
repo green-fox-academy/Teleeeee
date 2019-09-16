9ec6bd5b95ac937091f32900d70bd3e8af225bcc
Levente Paplogo <lev9433@gmail.com>
2019-09-16 17:28:08 +0200

    product databease 2 done

--- week--02/day--01/productList/main.cpp
+++ week--02/day--01/productList/main.cpp
@@ -47,7 +47,7 @@ int productsBelow300(std::map<std::string, double> below300){
 
     return j;
 }
-std::string anythingFor125(std::map<std::string, double> for125){
+void anythingFor125(std::map<std::string, double> for125){
     bool anyfor125;
     for (std::pair<std::string, double> tempProduct : for125) {
         if(tempProduct.second == 125){
@@ -65,7 +65,47 @@ std::string anythingFor125(std::map<std::string, double> for125){
 
 
 }
-std::string cheapestProduct(std::map<std::string, double> cheapestProduct);
+void cheapestProduct(std::map<std::string, double> cheapestProduct){
+    double cheapest = std::numeric_limits<double>::max();
+    std::string item;
+    for (std::pair<std::string, double> myPair : cheapestProduct) {
+        if( myPair.second < cheapest){
+            cheapest = myPair.second;
+        }
+    }
+    for (std::pair<std::string, double> myPair : cheapestProduct) {
+        if(myPair.second == cheapest){
+            item = myPair.first;
+        }
+    }
+    std::cout << item << std::endl;
+}
+void anythingUnder201(std::map<std::string, double> anythingunder){
+    std::vector<std::string> itemsUnder;
+    for (std::pair<std::string, double> tempProduct : anythingunder) {
+        if(tempProduct.second < 201){
+            itemsUnder.push_back(tempProduct.first);
+        }
+
+        }
+
+    for (std::string temp : itemsUnder) {
+        std::cout << temp << std::endl;
+    }
+
+
+
+
+}
+void productsOver300(std::map<std::string, double> anyover300){
+    std::vector<std::string> itemsOver;
+    for (std::pair<std::string, double> tempProduct : anyover300) {
+        if(tempProduct.second > 300){
+            std::cout << tempProduct.first << " " << tempProduct.second << std::endl;
+        }
+    }
+
+}
 
 
 
@@ -81,12 +121,18 @@ int main() {
     std::cout << "How much is the fish?" << std::endl;                                //How much is the fish?
 
     std::cout << "The fish's price is " << productList.find("Fish")->second << std::endl;
-
+    anythingFor125(productList);                                                         //Is there anything we can buy for exactly 125?
+    anythingUnder201(productList);                                                       //Which products cost less than 201? (just the name)
+    cheapestProduct(productList);
+    anythingFor125(productList);                                                         //Is there anything we can buy for exactly 125?
     std::cout << mostexpensive(productList) << std::endl;                                //What is the most expensive product?
     std::cout << averagePrice(productList) << std::endl;                                 //What is the average price?
     std::cout << productsBelow300(productList) << std::endl;                             //How many products' price is below 300?
-    anythingFor125(productList);                                                         //Is there anything we can buy for exactly 125?
-    cheapestProduct(productList);                                                         //What is the cheapest product?
+    productsOver300(productList);                                                        //Which products cost more than 150? (name + price)
+
+
+
+
 
 
 
@@ -96,21 +142,6 @@ int main() {
 
 }
 
-std::string cheapestProduct(std::map<std::string, double> cheapestProduct){
-    double cheapest = std::numeric_limits<double>::max();
-    std::string item;
-    for (std::pair<std::string, double> myPair : cheapestProduct) {
-        if( myPair.second < cheapest){
-            cheapest = myPair.second;
-        }
-    }
-    for (std::pair<std::string, double> myPair : cheapestProduct) {
-        if(myPair.second == cheapest){
-            item = myPair.first;
-        }
-    }
-    std::cout << item << std::endl;
-}
 
 
 
