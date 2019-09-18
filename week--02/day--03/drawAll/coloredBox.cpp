29cc4d4b76903637ca80e668c4c3cc121507ff6c
Levente Paplogo <lev9433@gmail.com>
2019-09-18 21:49:46 +0200

    some fix

--- week--02/day--03/drawAll/coloredBox.cpp
+++ week--02/day--03/drawAll/coloredBox.cpp
@@ -5,8 +5,7 @@
 #include "draw.h"
 
 void draw(SDL_Renderer* gRenderer) {
-    // Draw a box that has different colored lines on each edge.
-    // The center of the box should align with the center of the screen.
+
     SDL_SetRenderDrawColor(gRenderer, 0xff /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0x45 /*A*/);
     SDL_RenderDrawLine(gRenderer, 255, 210, 255, 270);
 
