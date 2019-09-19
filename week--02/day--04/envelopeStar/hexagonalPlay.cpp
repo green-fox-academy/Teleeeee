2c3601f84ee2779468e5beda719ba2efed7e51fa
Levente Paplogo <lev9433@gmail.com>
2019-09-19 14:43:16 +0200

    all project

--- week--02/day--04/envelopeStar/hexagonalPlay.cpp
+++ week--02/day--04/envelopeStar/hexagonalPlay.cpp
@@ -1,6 +1,9 @@
-
 #include "draw.h"
 
+
+
+
+//draws a moveable hexagon
 void drawHexagon(SDL_Renderer* gRenderer, int height , int shiftingPoint, int  shiftingPoint2) {
 
 
@@ -9,13 +12,13 @@ void drawHexagon(SDL_Renderer* gRenderer, int height , int shiftingPoint, int  s
 
 
 
-    SDL_SetRenderDrawColor(gRenderer, 0xFF /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0xFF /*A*/);
+    SDL_SetRenderDrawColor(gRenderer, 0 /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0 /*A*/);
     SDL_RenderDrawLine(gRenderer,
                        shiftingPoint + triangleHeight + upperLine,
                        shiftingPoint2 ,
                        shiftingPoint + triangleHeight,
                        shiftingPoint2 );
-    SDL_SetRenderDrawColor(gRenderer, 0xFF /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0xFF /*A*/);
+    SDL_SetRenderDrawColor(gRenderer, 0 /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0 /*A*/);
     SDL_RenderDrawLine(gRenderer,
                        shiftingPoint + triangleHeight + upperLine,
                        shiftingPoint2  + height,
@@ -23,13 +26,13 @@ void drawHexagon(SDL_Renderer* gRenderer, int height , int shiftingPoint, int  s
                        shiftingPoint2 + height);
 
 //side triangle
-    SDL_SetRenderDrawColor(gRenderer, 0xFF /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0xFF /*A*/);
+    SDL_SetRenderDrawColor(gRenderer, 0 /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0xFF /*A*/);
     SDL_RenderDrawLine(gRenderer,
                        shiftingPoint  + triangleHeight,
                        shiftingPoint2 ,
                        shiftingPoint ,
                        shiftingPoint2  + height / 2);
-    SDL_SetRenderDrawColor(gRenderer, 0xFF /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0xFF /*A*/);
+    SDL_SetRenderDrawColor(gRenderer, 0 /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0xFF /*A*/);
     SDL_RenderDrawLine(gRenderer,
                        shiftingPoint ,
                        shiftingPoint2  + height / 2,
@@ -37,13 +40,13 @@ void drawHexagon(SDL_Renderer* gRenderer, int height , int shiftingPoint, int  s
                        shiftingPoint2  +height);
 
 // other side triangle
-    SDL_SetRenderDrawColor(gRenderer, 0xFF /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0xFF /*A*/);
+    SDL_SetRenderDrawColor(gRenderer, 0 /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0xFF /*A*/);
     SDL_RenderDrawLine(gRenderer,
                        shiftingPoint  + upperLine + triangleHeight,
                        shiftingPoint2 ,
                        shiftingPoint  + 2 * upperLine,
                        shiftingPoint2  + height / 2);
-    SDL_SetRenderDrawColor(gRenderer, 0xFF /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0xFF /*A*/);
+    SDL_SetRenderDrawColor(gRenderer, 0 /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0xFF /*A*/);
     SDL_RenderDrawLine(gRenderer,
                        shiftingPoint  + 2 * upperLine,
                        shiftingPoint2  + height / 2,
@@ -56,12 +59,12 @@ void drawHexagon(SDL_Renderer* gRenderer, int height , int shiftingPoint, int  s
 void draw(SDL_Renderer* gRenderer) {
 
     int height = 60;
-    int downShift = height;
-    int sideShift = 108;
     int startPoint1 = 0; // shift to right
     int startPoint2 = 0; // shift to bottom
+    int shiftTheWholeThingHorizontal = 42;
+    int shiftTheWholeThingVertical = 120;
 
-    for (int i1 = 0; i1 <=1 ; ++i1) {
+    for (int i1 = 0; i1 <=1 ; ++i1) { // every for cycle is an column
 
         for (int n = 0; n <= 1; ++n) {
 
@@ -72,10 +75,10 @@ void draw(SDL_Renderer* gRenderer) {
                     for (int k = 0; k <= 1; ++k) {
 
                         for (int j = 0; j <= 1; ++j) {
-
-                            for (int i = 0; i < 4 + j + k + l - m - n - i1; ++i) {
-                                startPoint1 = 42 + (j * 54) + (k * 54) + (l * 54) + (m * 54) + (n * 54)+ (i1 * 54);
-                                startPoint2 = 120 - (j * 30) + (i * 60) - (k * 30) - (l * 30) + (m * 30) + (n * 30) + (i1 * 30);
+                            /*every cycle needs to change the number of hexagons in a coloumn*/
+                            for (int i = 0; i < 4 + j + k + l - m - n - i1 ;  ++i) {    // ( m+ n + i1) * (height /2)
+                                startPoint1 = shiftTheWholeThingHorizontal + (j + k + l + m + n + i1) * height * 0.9 ;  // every cycle shifts it to the right
+                                startPoint2 = shiftTheWholeThingVertical - (j + k + l) * (height /2) + (i * height) + (m+ n + i1) * (height /2); // (j + k + l) shifts upper way; (m+ n + i1) shifts it downer way
                                 drawHexagon(gRenderer, height, startPoint1, startPoint2);
                             }
                         }
@@ -84,6 +87,4 @@ void draw(SDL_Renderer* gRenderer) {
             }
         }
     }
-
-
 }
