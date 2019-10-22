cf06e2754d767649d7e70f51cf91d4ede8b81db5
Levente Paplogo <lev9433@gmail.com>
2019-10-22 16:51:52 +0200

    array rotator

--- week-06/day-02/array_rotation/main.cpp
+++ week-06/day-02/array_rotation/main.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
 
 void print_binary(uint8_t byte) {
     printf("%c%c%c%c %c%c%c%c ",
@@ -24,45 +23,74 @@ void array_rotator(void *ptr, uint32_t bytes, uint32_t rotation_count, uint8_t r
      *              | 1110 1010 | 1001 0101 | 0100 1000 |
      */
     uint8_t * pChar;
+
     pChar = (uint8_t *)ptr;
+
+    uint8_t  temp;
+
+    if(rotation_count >= 8){
+        uint32_t rotation_cirlce = rotation_count / 8;
+        for (int j = 0; j < rotation_cirlce ; ++j) {
+
+            uint8_t  temp_first = pChar[0];
+            uint8_t  temp_last = pChar[bytes - 1] ;
+
+            if (right > 0) {
+                for (uint8_t i = bytes - 1; i > 0; --i) {
+                    pChar[i] = pChar[i - 1];
+                } pChar[0] = temp_last;
+            }
+
+            if (right == 0) {
+                for (uint8_t i = 0; i < bytes - 1; ++i) {
+                    pChar[i] = pChar[i + 1];
+                }pChar[bytes - 1] = temp_first;
+            }
+
+        }
+    }
+
     uint16_t temp_first_element = pChar[0] | 0b0000000000000000;
     uint16_t temp_first_last = pChar[bytes -1]| 0b0000000000000000;
     uint16_t temp_first_first;
-    if(right > 0) {
-        pChar[0] = pChar[0] >> rotation_count | temp_first_last << (8 - rotation_count);
-        for (int i = 1; i < bytes; ++i) {
-            temp_first_first = pChar[i - 1] | 0b0000000000000000;
+
+    rotation_count = rotation_count % 8;
+    if (right > 0) {
+        for (int i = bytes - 1; i > 0; --i) {
+            temp_first_first = pChar[i-1] | 0b0000000000000000;
             pChar[i] = pChar[i] >> rotation_count | temp_first_first << (8 - rotation_count);
         }
+        pChar[0] = pChar[0] >> rotation_count | temp_first_last << (8 - rotation_count);
     }
-    if(right <= 0) {
-        pChar[bytes -1] = pChar[bytes -1] << rotation_count | temp_first_element >> (8 - rotation_count);
+    if (right <= 0) {
         for (int i = 0; i < bytes - 1; ++i) {
             temp_first_first = pChar[i + 1] | 0b0000000000000000;
             pChar[i] = pChar[i] << rotation_count | temp_first_first >> (8 - rotation_count);
         }
+        pChar[bytes - 1] = pChar[bytes - 1] << rotation_count | temp_first_element >> (8 - rotation_count);
     }
+
 }
 
 int main() {
 
-    uint8_t pChar[6] = {0xAA ,0x55 ,0xAA, 0x55,0x23 ,0x23};
+    uint8_t pChar[7] = {0xAA , 0x55 , 0x23, 0x59, 0x56, 0x53, 0x35};
 
-    for (int i = 0; i < 6 ; ++i) {
+    for (int i = 0; i < 7 ; ++i) {
         printf("| ");
         print_binary(pChar[i]);
         printf(" |");
     }
     printf("\n");
-    array_rotator(pChar, 6, 2, 1);
-    for (int i = 0; i < 6 ; ++i) {
+    array_rotator(pChar, 7, 65, 1);
+    for (int i = 0; i < 7 ; ++i) {
         printf("| ");
         print_binary(pChar[i]);
         printf(" |");
     }
     printf("\n");
-    array_rotator(pChar, 6, 2, 0);
-    for (int i = 0; i < 6 ; ++i) {
+    array_rotator(pChar, 7, 65, 0);
+    for (int i = 0; i < 7 ; ++i) {
         printf("| ");
         print_binary(pChar[i]);
         printf(" |");
