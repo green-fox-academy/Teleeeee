b19e6fb934bdc8ec7f0027ce53a8ecef266a19be
Levente Paplogo <lev9433@gmail.com>
2019-11-26 12:43:43 +0100

    ittvan mindenkinek lol

---
+++ week-10/day-01/RPM_measurement/atmel-template-project-master/atmel-template-project/freq_meas.c
@@ -0,0 +1,44 @@
+#include <stdint.h>
+#include <avr/interrupt.h>
+#include "freq_meas.h"
+
+int timer_first_state = 0;
+int timer_second_state = 0;
+volatile uint8_t timer_overflow_number = 0;
+uint8_t timer_capture_state = 0;
+double previous_frequency = 0;
+double steps = 0;
+
+// TODO:
+// Write the interrupt handlers
+
+ISR(TIMER1_OVF_vect){
+	timer_overflow_number++;
+}
+
+ISR(TIMER1_CAPT_vect){
+	timer_second_state = ICR1;
+	steps = (timer_overflow_number * 65535) + (timer_second_state - timer_first_state);
+	timer_first_state = ICR1;
+	timer_overflow_number = 0;
+}
+
+void freq_meas_init()
+{
+	TCCR1B = 0b11000010;
+	TIMSK1 = 0b00100001;
+	/**************
+	 * TC1 CONFIG *
+	 **************/
+	// TODO:
+	// Configure the TC1 timer properly :)
+}
+
+// TODO:
+// Write this function. It returns the measured frequency in Hz
+float get_freq()
+{
+	double period = ( 0.0000005 * steps);
+	previous_frequency = 1.00 / period;
+	return (float)previous_frequency;
+}
---
+++ week-10/day-01/RPM_measurement/atmel-template-project-master/atmel-template-project/freq_meas.h
@@ -0,0 +1,8 @@
+#include <avr/io.h>
+#ifndef _FREQ_MEAS_H
+#define _FREQ_MEAS_H
+
+void freq_meas_init();
+float get_freq();
+
+#endif // _FREQ_MEAS_H
