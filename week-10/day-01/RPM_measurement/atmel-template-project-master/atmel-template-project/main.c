f29de5a90ed7e70195fe2b764791f5d00663fd9b
Levente Paplogo <lev9433@gmail.com>
2019-11-27 15:13:26 +0100

    fan rpm P control done

--- week-10/day-01/RPM_measurement/atmel-template-project-master/atmel-template-project/main.c
+++ week-10/day-01/RPM_measurement/atmel-template-project-master/atmel-template-project/main.c
@@ -17,8 +17,9 @@
 #define LED_PORT		PORTB
 #define LED_PORT_POS	PORTB5
 
-uint8_t duty = 0;
+uint8_t duty = 100;
 uint16_t all_result = 0;
+uint16_t ref = 0;
 
 void system_init(){
 	LED_DDR |= 1 << LED_DDR_POS;
@@ -28,9 +29,7 @@ void system_init(){
 	sei();
 }
 
-ISR(ANALOG_COMP_vect){
-	PINC =  1 << 0;
-}
+ISR(ANALOG_COMP_vect){}
 
 void init_ADC(){
 	ADMUX = 0b00000000;
@@ -45,13 +44,22 @@ ISR(ADC_vect){
 	result_ADCL = ADCL;
 	result_ADCH = ADCH;
 	all_result = (((uint16_t)result_ADCH << 8) | result_ADCL);	
-	duty = (all_result/ 10) ;
-	set_duty(duty);
+	ref = 7 * all_result;
 }
+
+void P_controller(uint16_t disired_value_rpm, uint16_t actual_rpm){
+	if(disired_value_rpm > actual_rpm && duty < 100 ){
+		duty++;
+	}
+	if (disired_value_rpm < actual_rpm && duty > 1){
+		duty--;
+	}
+};
+
 
 int main(void)
 {
-	
+	char buffer[8];	
 	// Don't forget to call the init function :)
 	system_init();
 	init_ADC();
@@ -63,10 +71,12 @@ int main(void)
 	while (1) {
 		// Generating an about 1Hz signal on the LED pin.
 		// The printf call will also take some time, so this won't be exactly 1Hz.
-		_delay_ms(1000);
-		printf("%f Hz\n", get_freq());
+		for (uint8_t i = 0; i < 100; i++ ){
+			P_controller(ref, get_rpm());
+			set_duty(duty);
+			printf("%f RPM\n", get_rpm());
+			_delay_ms(100);
+		}
 		printf("%f RPM\n", get_rpm());
-		set_duty(50);
-		printf("%d \n", duty);
 	}
 }
